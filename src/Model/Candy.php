c04282a9d28f9a71dfc9c7b00c29b728e2f64edd
Florence LG <flo.lg.gautier@gmail.com>
2018-10-30 17:51:23 +0100

    fonction get et set

--- src/Model/Candy.php
+++ src/Model/Candy.php
@@ -11,5 +11,55 @@ namespace Model;
 
 class Candy
 {
+    private $id;
+    private $name;
+    private $id_ean;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEan()
+    {
+        return $this->id_ean;
+    }
+
+    /**
+     * @param mixed $id_ean
+     */
+    public function setIdEan($id_ean)
+    {
+        $this->id_ean = $id_ean;
+    }
 }
