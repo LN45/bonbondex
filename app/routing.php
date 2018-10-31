0e39e4c452ebf71722f72a8f1f374c9b7725aec1
Helene Bloch <ln-45@live.fr>
2018-10-31 01:56:06 +0100

    first commit helene

--- app/routing.php
+++ app/routing.php
@@ -10,11 +10,8 @@
  */
 
 $routes = [
-    'Item' => [ // Controller
-        ['index', '/', 'GET'], // action, url, method
-        ['add', '/item/add', ['GET', 'POST']], // action, url, method
-        ['edit', '/item/edit/{id:\d+}', ['GET', 'POST']], // action, url, method
-        ['show', '/item/{id:\d+}', 'GET'], // action, url, method
-        ['delete', '/item/delete/{id:\d+}', 'GET'], // action, url, method
-    ],
+    'Candy' => [
+        ['show','/candy/show','GET'],
+        ['add','/candy/add',['GET', 'POST']],
+    ]
 ];
